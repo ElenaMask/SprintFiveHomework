d8d94a50c4448a060fe42f0d7851fb346989b5f1
ElenaMask <Elenamask007@yandex.ru>
2024-05-28 21:28:13 +0300

    sixth iteration

--- ftracker.go
+++ ftracker.go
@@ -104,7 +104,7 @@ func WalkingSpentCalories(action int, duration, weight, height float64) float64 
 	// ваш код здесь
 	averageSpeedMsec := meanSpeed(action, duration) * kmhInMsec
 
-	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow(averageSpeedMsec, 2)/height/cmInM)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
+	return ((walkingCaloriesWeightMultiplier*weight + ((math.Pow(averageSpeedMsec, 2) / height / cmInM) * walkingSpeedHeightMultiplier * weight)) * duration * minInH)
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
